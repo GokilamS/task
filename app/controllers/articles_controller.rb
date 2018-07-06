b034648a30d8d4284a83b1ad235eaaba868a6396
Gokilam.S <gokilamks01@gmail.com>
2018-07-06 17:16:09 +0530

    third commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,22 +3,15 @@ def new
 	@article=Article.new
 end
 def create
-	 #render plain: params[:signup].inspect
-	 #byebug
 	 @article = Article.new(signup_params)
-  	# @article.save
   	if @article.save
      redirect_to @article
  else 
- 	#redirect_to :action => "index"
- 	#redirect_to root_url
  	render "sign"
  end
 end
-def v
-	end
 private
 	def signup_params
-    params.require(:signup).permit(:Username, :Email, :Password, :ConfirmPassword)
+    params.require(:signup).permit(:UserName, :Email, :Password, :ConfirmPassword)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,26 @@
-class Article < ApplicationRecord
-	 validates :UserName, presence: true, length: {minimum: 20}
-     validates :Email, presence: true, length: {minimum: 20}
-     validates :Password, presence: true, length: {minimum: 12} 
-     validates :UserName, presence: true, length: {minimum: 12}  
+class Article < ApplicationRecord  
+  attr_accessor :password
+  before_save :encrypt_password
+  
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :Email
+  validates_uniqueness_of :Email
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+	 
 end
